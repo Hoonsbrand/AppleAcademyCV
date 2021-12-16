d38f9a20ec034247ff0720fcd16c5492b5eb98f3
Hoonsbrand <hoonav3378@gmail.com>
2021-12-16 19:16:30 +0900

    test

--- Udemy IOS/I Am Poor/I Am Poor/ViewController.swift
+++ Udemy IOS/I Am Poor/I Am Poor/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Now I Am Poor.
     }
 
 
